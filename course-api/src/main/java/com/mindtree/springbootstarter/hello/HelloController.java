fbcd739153857df324938a91fcb57d061055b0c2
madhupriyamurali <50649243+madhupriyamurali@users.noreply.github.com>
2019-05-16 21:52:35 +0530

    Add files via upload

---
+++ course-api/src/main/java/com/mindtree/springbootstarter/hello/HelloController.java
@@ -0,0 +1,20 @@
+package com.mindtree.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindtree.springbootstarter.topic.Topic;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public Topic sayHi()
+	{
+		Topic t =new Topic();
+		t.setId("01");
+		t.setDescription("Spring Boot with Jenkins");
+		t.setName("Java");
+		return t;
+	}
+}
---
+++ course-api/src/test/java/com/mindtree/test/TestCoreApp.java
@@ -0,0 +1,19 @@
+package com.mindtree.test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.mindtree.springbootstarter.CourseApiApp;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes={CourseApiApp.class})
+
+@SpringBootTest
+public class TestCoreApp {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
